82301fd95be0ed26d384a6260b645408c3ac1171
Nishant kumar <nishantkumarfps@gmail.com>
2024-11-18 23:36:57 +0530

    Solved the Robin Helps problem (https://codeforces.com/contest/2014/problem/A)

---
+++ Codeforces div3(974)/ques1.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    n,k = map(int,input().split())
+    arr = list(map(int,input().split()))
+    score =0
+    ans=0
+    for i in range(n):
+        if arr[i]>=k:
+            score+=arr[i]
+        if arr[i]==0 and score >0:
+            ans+=1
+            score-=1
+                
+    print(ans)
+    
