3de8b0aada268c8d2046941b993c1d6089503bd4
Nishant kumar <nishantkumarfps@gmail.com>
2024-12-17 19:36:40 +0530

    Solved the potd of 17th december

---
+++ Leetcode/Problem Of The Day/december17.py
@@ -0,0 +1,3 @@
+#Question : Final Array State After K Multiplication Operations I
+#Link to the question: https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/description/?envType=daily-question&envId=2024-12-17
+
