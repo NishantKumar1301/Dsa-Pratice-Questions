2c456312188b8b9ed97f2d5c72bfa353b4d61d10
Nishant kumar <nishantkumarfps@gmail.com>
2024-12-19 01:27:46 +0530

    Solved the two red blue sort problem

--- Codechef/Starter 161/ques4.py
+++ Codechef/Starter 161/ques4.py
@@ -1,4 +1,23 @@
-#Question :
-#Link to the question: 
+#Question : Red-Blue Sort
+#Link to the question: https://www.codechef.com/problems/RBGM
+
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    cnt =0
+    for i in range(n):
+        if arr[i]==i+1:
+            cnt+=1
+    
+    if cnt == n:
+        print(n)
+    elif cnt >=1:
+        print(n-1)
+    else:
+        if n>=2 :
+            print(n-2)
+        else:
+            print(0)
 
 
