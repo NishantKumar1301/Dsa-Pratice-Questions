0a239864743032d804e3f1abe067a0f987cdf37d
Nishant kumar <nishantkumarfps@gmail.com>
2024-12-19 12:12:34 +0530

    Solved the potd of 19th december

--- Leetcode/Problem Of The Day/december19.py
+++ Leetcode/Problem Of The Day/december19.py
@@ -1,3 +1,19 @@
-#Question :
-#Link to the question: 
+#Question : Max Chunks To Make Sorted 
+#Link to the question: https://leetcode.com/problems/max-chunks-to-make-sorted/description/?envType=daily-question&envId=2024-12-19
+
+class Solution(object):
+    def maxChunksToSorted(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        maxi = float("-inf")
+        ans =0
+        for i in range(len(arr)):
+            maxi = max(maxi,arr[i])
+            if i==maxi:
+                ans+=1
+        return ans
+
+        
 
