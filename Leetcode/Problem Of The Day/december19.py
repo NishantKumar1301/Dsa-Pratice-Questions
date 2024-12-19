fc1bea9b6b26ab034857e9586286fdf1206ae844
Nishant kumar <nishantkumarfps@gmail.com>
2024-12-19 14:34:34 +0530

    Modified the previous solution with the optimal way

--- Leetcode/Problem Of The Day/december19.py
+++ Leetcode/Problem Of The Day/december19.py
@@ -7,13 +7,25 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
-        maxi = float("-inf")
-        ans =0
-        for i in range(len(arr)):
-            maxi = max(maxi,arr[i])
-            if i==maxi:
-                ans+=1
-        return ans
+        #Approach 1: If the array element is all different
+        # maxi = float("-inf")
+        # ans =0
+        # for i in range(len(arr)):
+        #     maxi = max(maxi,arr[i])
+        #     if i==maxi:
+        #         ans+=1
+        # return ans
+        
+        #Approach 2: Optimal => If there is duplicate in the array
+        
+        stack =[]
+        for num in arr:
+            largest = num 
+            while stack and num<stack[-1]:
+                largest = max(largest,stack.pop())
+            stack.append(largest)
+        return len(stack)
+
 
         
 
