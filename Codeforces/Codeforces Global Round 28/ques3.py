41f26e76459c15f3eb82b40313a81ed71aefb93d
Nishant kumar <nishantkumarfps@gmail.com>
2024-12-20 16:58:12 +0530

    Solved the kelvin and binary string question with modified approach

--- Codeforces/Codeforces Global Round 28/ques3.py
+++ Codeforces/Codeforces Global Round 28/ques3.py
@@ -1,29 +1,35 @@
 #Question :  Kevin and Binary Strings
 #Link to the question: https://codeforces.com/contest/2048/problem/C
 
-z = int(input())
-for _ in range(z):
+for _ in range(int(input())):
     s = input()
-    p = -1
     n = len(s)
-    res = 0
- 
+    index=-1 #Index will keep track of the first occurrence of 0
+    #Find the index of first occurrence of 0
     for i in range(n):
-        if s[i] == '0':
-            p = i
+        if s[i]=='0':
+            index = i
             break
-        else:
-            res += 1
- 
-    y1 = 0
-    if p != -1:
-        for i in range(p, n):
-            if s[i] == '0':
-                y1 += 1
+        
+    #Count the number of consecutive zeroes 
+    count =0
+    if index!=-1:
+        for i in range(n):
+            if s[i]=='0':
+                count+=1 
             else:
-                break
- 
-    if p == -1:
-        print(f"1 {len(s)} 1 1")
+                break 
+    
+    #Observation:  first substring will be the entire substring always
+    #if no zero is found then the second should contain only one 1 
+    #if zero is found then the second substring should start with max(0,index-count)+1 
+    #And ends at the index of min(index,count)
+    
+    ans =(0,0,0,0)
+    if index==-1:
+        ans =(1,n,1,1)
+        # print(f"1 {n} 1 1")
     else:
-        print(f"1 {len(s)} {max(0, p - y1) + 1} {n - min(p, y1)}")
+        ans =(1,n,max(index-count)+1,n-min(index,count))
+        # print(f"1 {n} {max(0,index-count)+1} {n-min(index,count)} ")
+    print(*ans)
