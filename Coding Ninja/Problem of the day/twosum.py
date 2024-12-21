fafac9094e6f8cc7d8cd1cf245b046b162b1703e
Nishant kumar <nishantkumarfps@gmail.com>
2024-12-21 23:48:11 +0530

    Added the 2sum problem

---
+++ Coding Ninja/Problem of the day/twosum.py
@@ -0,0 +1,18 @@
+#Question : 2Sum
+#Link to the question:https://www.naukri.com/code360/problems/reading_6845742?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_Arrayproblems
+
+def read(n: int, book: [int], target: int) -> str:
+    # Write your code here.
+    left = 0
+    right = n-1
+    book.sort()
+    while left<right:
+        sum_value = book[left]+book[right]
+        if sum_value==target:
+            return "YES"
+        elif sum_value<target:
+            left+=1
+        else:
+            right-=1
+    return "NO"
+
