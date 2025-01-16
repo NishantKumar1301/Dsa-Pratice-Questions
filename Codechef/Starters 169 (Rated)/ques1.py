4e0be5f5fb843b5680c2386088505e29c633568e
Nishant kumar <nishantkumarfps@gmail.com>
2025-01-16 12:07:33 +0530

    SOlved the first question of the contest

---
+++ Codechef/Starters 169 (Rated)/ques1.py
@@ -0,0 +1,11 @@
+#Question :Entry Check
+#Link to the question: https://www.codechef.com/problems/P1169
+
+# cook your dish here
+n = int(input())
+if n >=10:
+    print("YES")
+else:
+    print("NO")
+
+
