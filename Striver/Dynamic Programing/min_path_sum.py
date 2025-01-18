35b262f3eba95d05e13f893b83ca410f56e57a03
Nishant kumar <nishantkumarfps@gmail.com>
2025-01-19 01:53:10 +0530

    Solved the min path sum problem using tabulation approach

--- Striver/Dynamic Programing/min_path_sum.py
+++ Striver/Dynamic Programing/min_path_sum.py
@@ -17,14 +17,31 @@ class Solution(object):
         return dp[i][j]
 
     def minPathSum(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
         n ,m = len(grid),len(grid[0])
         dp = [[-1]*m for _ in range(n)]
         return self.helper(n-1,m-1,grid,dp)
 
-        
+#Method 2 : Using Tabulation
+
+def minSumPath(grid):
+    # Write your code here.
+    m,n = len(grid),len(grid[0])
+    dp =[[-1]*n for _ in range(m)]
+    for i in range(m):
+        for j in range(n):
+            if i==0 and j ==0:
+                dp[i][j]=grid[i][j]
+                continue
+            up=left = grid[i][j]
+            if i>0:
+                up+=dp[i-1][j]
+            else:
+                up+=float('inf')
+            if j>0:
+                left+=dp[i][j-1]
+            else:
+                left+=float('inf')
+            dp[i][j]=min(up,left)
+    return dp[m-1][n-1]
 
 
