d8d53a16c0e21092be75979c5aa614d404ccdd85
Nishant kumar <nishantkumarfps@gmail.com>
2025-01-18 23:38:53 +0530

    Solved the unique path problem by tabulation approach

--- Striver/Dynamic Programing/unique_path.py
+++ Striver/Dynamic Programing/unique_path.py
@@ -26,6 +26,23 @@ class Solution(object):
         return self.helper(m-1,n-1,dp)
         
         
-#
+#Method 2 : Using Tabulation 
+
+def uniquePaths(m, n):
+	# Write your code here.
+	dp =[[-1]*n for _ in range(m)]
+	dp[0][0]=1
+	for i in range(m):
+		for j in range(n):
+			if i==0 and j==0 :
+				continue
+			left,up=0,0
+			if i>0:
+				up= dp[i-1][j]
+			if j>0:
+				left = dp[i][j-1]
+			dp[i][j]=left+up 
+
+	return dp[m-1][n-1]
 
 
