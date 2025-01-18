9572dec33b5d5065b1fe3c0eb5b3c1bfea65aed6
Nishant kumar <nishantkumarfps@gmail.com>
2025-01-19 01:02:08 +0530

    SOlved the unique path2 problem using tabulation

--- Striver/Dynamic Programing/unique_path2.py
+++ Striver/Dynamic Programing/unique_path2.py
@@ -23,5 +23,27 @@ class Solution(object):
         n = len(obstacleGrid[0])
         dp =[[-1]*n for _ in range(m)]
         return self.helper(m-1,n-1,obstacleGrid,dp)
+    
+#Method2 : Using Tabulation
+
+MOD = 10**9 + 7
+def mazeObstacles(n, m, mat):
+    # Write your code here.
+    dp =[[-1]*m for _ in range(n)]
+    dp[0][0]=1
+    for i in range(n):
+        for j in range(m):
+            if i==0 and j==0:
+                continue
+            if i>=0 and j>= 0 and mat[i][j]==-1:
+                dp[i][j]=0
+                continue
+            up,left=0,0
+            if i>0: 
+                up=dp[i-1][j] % MOD
+            if j>0:
+                left=dp[i][j-1] % MOD
+            dp[i][j]=(left+up)%MOD
+    return dp[n-1][m-1]
 
 
