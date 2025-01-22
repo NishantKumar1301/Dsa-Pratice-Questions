3b1a10578d2d27e46d47fcc1cd985c3f90829253
Nishant kumar <nishantkumarfps@gmail.com>
2025-01-22 21:25:27 +0530

    Solved the first question of  contest

---
+++ Codechef/Starters 170 (Rated)/ques1.py
@@ -0,0 +1,11 @@
+#Question : Access Code Equality
+#Link to the question: https://www.codechef.com/START170B/problems/WECNITK
+
+# cook your dish here
+s= input()
+if s=="WECNITK":
+    print("Welcome to Web Club!")
+else:
+    print("Access denied")
+
+
