ba286026c836165f096854681c78ae769ed3df9b
Nishant kumar <nishantkumarfps@gmail.com>
2025-01-31 00:49:32 +0530

    Solved the first question of the contest

---
+++ Codechef/Starters 171 (Rated)/ques1.py
@@ -0,0 +1,7 @@
+#Question : Squid Game - Piggy Bank
+#Link to the question: https://www.codechef.com/problems/SQUIDBANK
+
+# cook your dish here
+n,k = map(int,input().split())
+print((n-k)*10000)
+
