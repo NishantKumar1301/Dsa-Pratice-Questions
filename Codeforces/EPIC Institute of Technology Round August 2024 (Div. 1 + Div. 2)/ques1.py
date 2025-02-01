510d19b9f3d636498b22407b296a1bb5f84182fd
Nishant kumar <nishantkumarfps@gmail.com>
2025-02-02 01:07:36 +0530

    SOlved the first question of the contest[https://codeforces.com/contest/2002/problem/A]

---
+++ Codeforces/EPIC Institute of Technology Round August 2024 (Div. 1 + Div. 2)/ques1.py
@@ -0,0 +1,6 @@
+#Question : Distanced Coloring
+#Link to the question: https://codeforces.com/contest/2002/problem/A
+
+for _ in range(int(input())):
+    n,m,k = map(int,input().split())
+    print(min(n,k)*min(m,k))
