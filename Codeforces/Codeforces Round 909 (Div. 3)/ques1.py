77ff73a4f2fae04ae5a8939a870d74abf9826d6e
Nishant Kumar <nishantkumarfps@gmail.com>
2025-02-06 00:52:24 +0530

    Solved the first question of the contest[https://codeforces.com/contest/1899/problem/A]

---
+++ Codeforces/Codeforces Round 909 (Div. 3)/ques1.py
@@ -0,0 +1,10 @@
+#Question : Game with Integers
+#Link to the question: https://codeforces.com/contest/1899/problem/A
+
+
+for _ in range(int(input())):
+    n = int(input())
+    if n%3==0:
+        print("SECOND")
+    else:
+        print("FIRST")
