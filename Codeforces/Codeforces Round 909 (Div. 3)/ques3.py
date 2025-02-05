e98df49018a396496aefc9ba0a1f392db04a9cf6
Nishant Kumar <nishantkumarfps@gmail.com>
2025-02-06 02:39:19 +0530

    Solved the third question of the contest[https://codeforces.com/contest/1899/problem/C]

---
+++ Codeforces/Codeforces Round 909 (Div. 3)/ques3.py
@@ -0,0 +1,16 @@
+#Question : Yarik and Array
+#Link to the question: https://codeforces.com/contest/1899/problem/C
+
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    ans,total = float('-inf'),0
+    for i in range(n):
+        total+=arr[i]
+        ans = max(ans,total)
+        if i<n-1 and (abs(arr[i])%2==abs(arr[i+1])%2):
+            total =0
+        if total<0:
+            total =0
+    print(ans)
