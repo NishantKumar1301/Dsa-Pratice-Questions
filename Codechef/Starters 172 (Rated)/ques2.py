f63c37f80ba4f92d06f61fd7d6e2b50e185e4719
Nishant Kumar <nishantkumarfps@gmail.com>
2025-02-05 22:53:56 +0530

    Solved the second  question of the contest

---
+++ Codechef/Starters 172 (Rated)/ques2.py
@@ -0,0 +1,12 @@
+#Question :  Small palindrome
+#Link to the question: https://www.codechef.com/problems/SMLPAL
+
+# cook your dish here
+for _ in range(int(input())):
+    x,y = map(int, input().split())
+    print("1" *(x//2) , end="")
+    print("2" *(y) , end="")
+    print("1" *(x//2) )
+    
+    
+
