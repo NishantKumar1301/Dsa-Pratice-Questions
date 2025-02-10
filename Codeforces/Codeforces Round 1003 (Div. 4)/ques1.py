a5098e4173c2913489167ff840b4c7217ba4834f
Nishant Kumar <nishantkumarfps@gmail.com>
2025-02-10 16:11:24 +0530

    Solved first question o the contest[https://codeforces.com/contest/2065/problem/A]

---
+++ Codeforces/Codeforces Round 1003 (Div. 4)/ques1.py
@@ -0,0 +1,16 @@
+#Question : Skibidus and Amog'u
+#Link to the question: https://codeforces.com/contest/2065/problem/A
+
+# Contest: Codeforces Round 1003 (Div. 4)
+# Date: February 9, 2025
+# Author: Nishant Kumar
+
+def solve():
+    n = input()
+    print(n[:-2] + "i")  
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        solve()
+
+# Author: Nishant Kumar
