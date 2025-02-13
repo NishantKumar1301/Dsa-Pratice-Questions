4c2b16dad394694fc17c7d12913ada2cbcd603de
Nishant Kumar <nishantkumarfps@gmail.com>
2025-02-13 21:48:15 +0530

    Solved the first question of the contest[https://codeforces.com/contest/2032/problem/B]

---
+++ Codeforces/Codeforces Round 983 (Div. 2)/ques1.py
@@ -0,0 +1,11 @@
+#Question : Circuit
+#Link to the question: https://codeforces.com/contest/2032/problem/B
+
+
+for _ in range(int(input())):
+    n = int(input())
+    arr= list(map(int,input().split()))
+    zero,one = arr.count(0),arr.count(1)
+    mini = one%2 
+    maxi = min(zero,one)
+    print(mini,maxi)
