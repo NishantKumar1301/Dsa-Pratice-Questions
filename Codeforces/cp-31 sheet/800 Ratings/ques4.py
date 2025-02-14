40fe59eb8c1e2d62270a919ac4a0998c9c93c2bf
Nishant Kumar <nishantkumarfps@gmail.com>
2025-02-14 16:55:17 +0530

    Solved the game with integer  problem

---
+++ Codeforces/cp-31 sheet/800 Ratings/ques4.py
@@ -0,0 +1,10 @@
+#Question : Game with integers
+#Link to the question: https://codeforces.com/problemset/problem/1899/A
+
+for _ in range(int(input())):
+    n = int(input())
+    if n%3==0:
+        print("Second")
+    else:
+        print("First")
+
