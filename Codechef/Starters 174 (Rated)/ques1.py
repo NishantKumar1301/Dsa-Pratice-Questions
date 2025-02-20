e7357bf96f82186160c56c31e928aee4b34e21db
Nishant Kumar <nishantkumarfps@gmail.com>
2025-02-20 15:50:30 +0530

    Solved the first question of the contest

---
+++ Codechef/Starters 174 (Rated)/ques1.py
@@ -0,0 +1,7 @@
+#Question : Too much hardwork
+#Link to the question: https://www.codechef.com/problems/HWFIN
+
+# cook your dish here
+x,y = map(int,input().split())
+print("YES" if x+10*y>=100 else "NO")
+
