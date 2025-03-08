32b57b5c73026385d6f5caf8b72e48142cb04d59
Nishant Kumar <nishantkumarfps@gmail.com>
2025-03-08 14:46:51 +0530

    SOlved the Longest Divisors Interval[https://codeforces.com/problemset/problem/1855/B]

--- Codeforces/cp-31 sheet/900 Ratings/ques6.py
+++ Codeforces/cp-31 sheet/900 Ratings/ques6.py
@@ -3,14 +3,14 @@
 
 import math
 
-a = 1
-for i in range(2, 51):
-    a = math.lcm(a, i)
-print(a)
+# a = 1
+# for i in range(2, 51):
+#     a = math.lcm(a, i)
+# print(a)
 
 for _ in range(int(input())):
     n = int(input())
     i = 1
     while n % i == 0:
         i += 1
-    print(i )
+    print(i-1 )
