a77831b8170f50aeb46e4c2c6d48669a7078b7e8
Nishant Kumar <nishantkumarfps@gmail.com>
2025-03-08 14:05:29 +0530

    Solved the make it zero problem

---
+++ Codeforces/cp-31 sheet/900 Ratings/ques3.py
@@ -0,0 +1,17 @@
+#Question : Make It Zero
+#Link to the question: https://codeforces.com/problemset/problem/1869/A
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    if n % 2 == 0: 
+        print(2)
+        print(1, n)
+        print(1, n)
+    else:  
+        print(4)
+        print(1, n - 1)
+        print(1, n - 1)
+        print(n - 1, n)
+        print(n - 1, n)
