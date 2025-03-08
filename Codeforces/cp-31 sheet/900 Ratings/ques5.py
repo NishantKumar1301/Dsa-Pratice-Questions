86811ec649d9b188e88647141faf9cca68371877
Nishant Kumar <nishantkumarfps@gmail.com>
2025-03-08 14:34:27 +0530

    Solved the chemistry problem[https://codeforces.com/problemset/problem/1883/B]

---
+++ Codeforces/cp-31 sheet/900 Ratings/ques5.py
@@ -0,0 +1,19 @@
+#Question : Chemistry
+#Link to the question: https://codeforces.com/problemset/problem/1883/B
+
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    s = input()
+
+    freq = [0] * 26
+
+    for char in s:
+        freq[ord(char) - ord('a')] += 1
+
+    odd = sum(1 for count in freq if count % 2 == 1)
+
+    if odd > k + 1:
+        print("NO")
+    else:
+        print("YES")
