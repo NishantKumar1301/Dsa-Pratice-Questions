3f7dd7638e0ceea01b9963b0026aefce17bd5e80
Nishant Kumar <nishantkumarfps@gmail.com>
2025-03-26 23:25:57 +0530

    Solved the first question of the contest

---
+++ Codechef/Starters 179 (Rated)/ques1.py
@@ -0,0 +1,10 @@
+#Question : Conquer the Fest!!
+#Link to the question: https://www.codechef.com/problems/CLSI?tab=statement
+
+# cook your dish here
+a,b = map(int,input().split())
+if 10*b<=a:
+    print("YES")
+else:
+    print("NO")
+
