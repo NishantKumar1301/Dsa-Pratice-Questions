fe10bab333e8a6649e947c93ad91238b77362fe3
Nishant Kumar <nishantkumarfps@gmail.com>
2025-03-30 20:58:29 +0530

    Solved the first question of the contest

--- Leetcode/Contest/Biweekly Contest 153/ques2.py
+++ Leetcode/Contest/Biweekly Contest 153/ques2.py
@@ -1,3 +1,37 @@
 #Question : Maximize Active Section with Trade I
 #Link to the question: https://leetcode.com/problems/maximize-active-section-with-trade-i/description/
 
+
+class Solution(object):
+    def maxActiveSectionsAfterTrade(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        one = s.count('1')
+        s = '1' + s + '1'
+        m = n+2
+        ans = one
+        
+        start = 1
+        while start < m - 1:
+            if s[start] == '1':
+                j = start
+                while j < m and s[j] == '1':
+                    j += 1
+                if j < m - 1 and s[start- 1] == '0' and s[j] == '0':
+                    a, c = 0, 0
+                    k = start - 1
+                    while k >= 0 and s[k] == '0':
+                        a += 1
+                        k -= 1
+                    k = j
+                    while k < m and s[k] == '0':
+                        c += 1
+                        k += 1
+                    ans = max(ans, one + a + c)
+                start = j
+            else:
+                start += 1
+        return ans
