b2ffc9021fb1a438c3716b1efeda7ed9df7a214d
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-01 23:54:08 +0530

    Solved the potd of 1st April

--- Leetcode/Problem Of The Day/april1.py
+++ Leetcode/Problem Of The Day/april1.py
@@ -1,3 +1,22 @@
-#Question :
-#Link to the question: 
+#Question : Solving Questions With Brainpower
+#Link to the question: https://leetcode.com/problems/solving-questions-with-brainpower/description/?envType=daily-question&envId=2025-04-01
 
+class Solution(object):
+    def solve(self,i,n,grid,dp):
+        if i>=n:
+            return 0
+        if dp[i]!=-1:
+            return dp[i]
+        not_take = self.solve(i+1,n,grid,dp)
+        take = self.solve(i+grid[i][1]+1,n,grid,dp)+grid[i][0]
+        dp[i]= max(take,not_take)
+        return dp[i]
+
+    def mostPoints(self, questions):
+        """
+        :type questions: List[List[int]]
+        :r type: int
+        """
+        n = len (questions)
+        dp = [-1]*n
+        return self.solve(0,n,questions,dp)
