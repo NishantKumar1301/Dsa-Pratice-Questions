9c99eed395f9de68ab2006c73aa32062287baa22
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-02 21:03:03 +0530

    SOlved the first question of the contest

---
+++ Codechef/Starters 180 (Rated)/ques1.py
@@ -0,0 +1,10 @@
+#Question : CodeMat
+#Link to the question: https://www.codechef.com/START180B/problems/CDMT
+
+# cook your dish here
+x,y = map(int,input().split())
+if y>x:
+    print("YES")
+else:
+    print("NO")
+
