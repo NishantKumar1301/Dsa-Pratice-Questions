e5c3d251c1e482088085457f117f1d926a61551c
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-08 21:27:17 +0530

    Solved the first problem of the contest

---
+++ Codeforces/Codeforces Round 1016 (Div. 3)/ques1.py
@@ -0,0 +1,10 @@
+#Question : Ideal Generator
+#Link to the question: https://codeforces.com/contest/2093/problem/A
+
+for _ in range(int(input())):
+    n = int(input())
+    if n%2==0:
+        print("NO")
+    else:
+        print("YES")
+
