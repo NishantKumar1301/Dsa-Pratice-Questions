16d1e45d4afaaf95aa8333b8015ffad8c8b0b3f3
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-09 17:24:34 +0530

    Solved the potd of 9th april

--- Leetcode/Problem Of The Day/April 2025/april9.py
+++ Leetcode/Problem Of The Day/April 2025/april9.py
@@ -1,7 +1,6 @@
 #Question : Minimum Operations to Make Array Values Equal to K
 #Link to the question: https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k/description/?envType=daily-question&envId=2025-04-09
 
-
 class Solution(object):
     def minOperations(self, nums, k):
         s = set()
@@ -11,5 +10,4 @@ class Solution(object):
             elif num>k:
                 s.add(num)
         return len(s)
-        
 
