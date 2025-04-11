2111524a1b4d68f4507efc62980899247d8c6eae
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-11 20:28:01 +0530

    Solved the potd of 10th april

--- Leetcode/Problem Of The Day/April 2025/april10.py
+++ Leetcode/Problem Of The Day/April 2025/april10.py
@@ -1,3 +1,33 @@
-#Question :
-#Link to the question: 
+#Question :Count the Number of Powerful Integers
+#Link to the question: https://leetcode.com/problems/count-the-number-of-powerful-integers/description/?envType=daily-question&envId=2025-04-10
 
+class Solution:
+    def numberOfPowerfulInt(
+        self, start, finish, limit, s
+    ):
+        start_ = str(start - 1)
+        finish_ = str(finish)
+        return self.calculate(finish_, s, limit) - self.calculate(
+            start_, s, limit
+        )
+
+    def calculate(self, x, s, limit):
+        if len(x) < len(s):
+            return 0
+        if len(x) == len(s):
+            return 1 if x >= s else 0
+
+        suffix = x[len(x) - len(s) :]
+        count = 0
+        pre_len = len(x) - len(s)
+
+        for i in range(pre_len):
+            if limit < int(x[i]):
+                count += (limit + 1) ** (pre_len - i)
+                return count
+            count += int(x[i]) * (limit + 1) ** (pre_len - 1 - i)
+
+        if suffix >= s:
+            count += 1
+
+        return count
