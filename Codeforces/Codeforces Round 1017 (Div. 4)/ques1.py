1335e5fc6c7c72b4267c00a6869a906890af3ff8
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-13 22:25:51 +0530

    Solved the first problem of the contest [https://codeforces.com/contest/2094/problem/A]

---
+++ Codeforces/Codeforces Round 1017 (Div. 4)/ques1.py
@@ -0,0 +1,20 @@
+#Question : Trippi Troppi
+#Link to the question: https://codeforces.com/contest/2094/problem/A
+
+# Contest: Codeforces Round 1017 (Div. 4)
+# Date: April 13, 2025
+# Author: Nishant Kumar
+
+def solve():
+    s= input().split(' ')
+    ans = ""
+    for char in s:
+        ans  = ans + char[0]
+    print(ans)
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        solve()
+
+# Author: Nishant Kumar
+
