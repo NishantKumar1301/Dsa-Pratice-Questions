62ddaf458c11399f9fa4f038fe9f7dfe9fa3e1e3
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-13 22:38:25 +0530

    Solved the third problem of the contest [https://codeforces.com/contest/2094/problem/C]

--- Codeforces/Codeforces Round 1017 (Div. 4)/ques3.py
+++ Codeforces/Codeforces Round 1017 (Div. 4)/ques3.py
@@ -1,5 +1,35 @@
 #Question : Brr Brrr Patapim
 #Link to the question: https://codeforces.com/contest/2094/problem/C
 
+# Contest: Codeforces Round 1017 (Div. 4)
+# Date: April 13, 2025
+# Author: Nishant Kumar
+ 
+def solve():
+    n = int(input())
+    grid = [list(map(int,input().split())) for _ in range(n)]
+    dp=[0]*(2*n+1)
+    for i in range(n):
+        for j in range(n):
+            total = i+j+2
+            if dp[total]==0:
+                dp[total]=grid[i][j]
+    
+    visited = [False]*(2*n+1)
+    for i in range(2*n+1):
+        visited[dp[i]]=True
+    
+    ans =0
+    for i in range(1,2*n+1):
+        if not visited[i]:
+            ans = i
+            break
+    dp[1]=ans
+    print(" ".join(map(str, dp[1:2 * n + 1])))
+    
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        solve()
 
+# Author: Nishant Kumar
 
