65bf0835ebed21159fbe3d6858186dc118b08b2c
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-14 14:41:07 +0530

    Solved the potd of 14th april

--- Leetcode/Problem Of The Day/April 2025/april14.py
+++ Leetcode/Problem Of The Day/April 2025/april14.py
@@ -1,5 +1,22 @@
 #Question : Count Good Triplets
 #Link to the question: https://leetcode.com/problems/count-good-triplets/description/?envType=daily-question&envId=2025-04-14
 
-
+class Solution(object):
+    def countGoodTriplets(self, arr, a, b, c):
+        """
+        :type arr: List[int]
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: int
+        """
+        ans = 0
+        n = len(arr)
+        for i in range(n-2):
+            for j in range(i+1,n-1):
+                for k in range(j+1,n):
+                    if abs(arr[i]-arr[j])<=a and abs(arr[j]-arr[k])<=b and abs(arr[i]-arr[k])<=c:
+                        ans+=1
+        return ans
+        
 
