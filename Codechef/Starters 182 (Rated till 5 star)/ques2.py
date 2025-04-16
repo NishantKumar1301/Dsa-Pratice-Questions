1c1a6b8f8f21f1b71fb49d921daab814692587da
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-16 20:16:52 +0530

    Solved the second question of the contest

---
+++ Codechef/Starters 182 (Rated till 5 star)/ques2.py
@@ -0,0 +1,11 @@
+#Question :  IPL
+#Link to the question: https://www.codechef.com/START182B/problems/INPL
+
+# cook your dish here
+n = int(input())
+if n>=7:
+    print("THALA")
+else:
+    print("BOOM")
+    
+
