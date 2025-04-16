2623efdc1cec0a1c8c14952cfbe48a6aa636712d
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-16 20:14:36 +0530

    Solved the first question of the contest

---
+++ Codechef/Starters 182 (Rated till 5 star)/ques1.py
@@ -0,0 +1,18 @@
+#Question : Bar Queue
+#Link to the question: https://www.codechef.com/START182B/problems/QBGI
+
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s= input()
+    b = g= 0
+    for i in range(n):
+        if b>2*g:
+            break
+        if s[i]=='B':
+            b+=1
+        else:
+            g+=1
+        
+    print(b+g)
+
