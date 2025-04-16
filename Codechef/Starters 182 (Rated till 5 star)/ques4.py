2b114375023213a7b12d932500d1321c0c749546
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-16 20:48:42 +0530

    Solved the fourth question of the contest

--- Codechef/Starters 182 (Rated till 5 star)/ques4.py
+++ Codechef/Starters 182 (Rated till 5 star)/ques4.py
@@ -1,3 +1,19 @@
 #Question : Palindromic Score
 #Link to the question: https://www.codechef.com/START182B/problems/LPSS
 
+# cook your dish here
+def helper(x,y):
+    if x%2==0 or y%2==0:
+        return x+y 
+    return x+y-1
+
+def solve(x,y,z):
+    a = helper(y,z)
+    b = helper(x,z)
+    c = helper(x,y)
+    return min(a,b,c)
+
+for _ in range(int(input())):
+    x,y,z = map(int,input().split())
+    ans = solve(x,y,z)
+    print(ans)
