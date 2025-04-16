4a5e5e354b904f95e8b00f55b828508c345d68e8
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-16 20:34:13 +0530

    Solved the third question of the contest

--- Codechef/Starters 182 (Rated till 5 star)/ques3.py
+++ Codechef/Starters 182 (Rated till 5 star)/ques3.py
@@ -1,3 +1,16 @@
 #Question : Beautiful Garden
 #Link to the question: https://www.codechef.com/START182B/problems/DPF
 
+# cook your dish here
+for _ in range(int(input())):
+    n,k,d = map(int,input().split())
+    arr = list(map(int,input().split()))
+    if n<=k:
+        print(0)
+    else:
+        arr.sort()
+        ans = 0
+        for i in range(n-k):
+            ans+=(d-1)//arr[i]+ 1 
+        print(ans)
+
