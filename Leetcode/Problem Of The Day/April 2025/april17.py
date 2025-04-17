86970d5e77c65e55db9d81fdffcdd449e390dc29
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-17 15:17:45 +0530

    SOlved the potd of 17th april

---
+++ Leetcode/Problem Of The Day/April 2025/april17.py
@@ -0,0 +1,5 @@
+#Question : Count Equal and Divisible Pairs in an Array
+#Link to the question: https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/description/?envType=daily-question&envId=2025-04-17
+
+
+
