dbc7d3a8f52679353bf8eb47ab8e96fe9dccaa7d
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-17 15:18:09 +0530

    SOlved the potd of 17th april

--- Leetcode/Problem Of The Day/April 2025/april17.py
+++ Leetcode/Problem Of The Day/April 2025/april17.py
@@ -1,5 +1,19 @@
 #Question : Count Equal and Divisible Pairs in an Array
 #Link to the question: https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/description/?envType=daily-question&envId=2025-04-17
 
-
+class Solution(object):
+    def countPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        ans =0
+        for i in range(n):
+            for j in range(i,n):
+                if nums[i]==nums[j]:
+                    if (i*j) % k==0 and i<j:
+                        ans+=1
+        return ans
 
