be5548952fbb60bf2d9076644b66db4be383fef0
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-18 18:26:03 +0530

    Solved the potd of 18th april

--- Leetcode/Problem Of The Day/April 2025/april18.py
+++ Leetcode/Problem Of The Day/April 2025/april18.py
@@ -1,3 +1,25 @@
-#Question :
-#Link to the question : 
+#Question : Count and Say
+#Link to the question : https://leetcode.com/problems/count-and-say/description/?envType=daily-question&envId=2025-04-18
+
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        ans = ""
+        if n==1:
+            return "1"
+        i=0
+        s = self.countAndSay(n-1)
+        while i<len(s):
+            char = s[i]
+            cnt = 1
+            while i < len(s)-1 and s[i]==s[i+1]:
+                cnt+=1
+                i+=1
+            ans += str(cnt)+char
+            i+=1
+        return ans
+
 
