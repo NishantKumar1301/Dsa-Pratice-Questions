d9fd4a8b45e27ea8fefd779a1a962f96495258be
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-19 15:34:54 +0530

    SOlved the potd of 19th april

--- Leetcode/Problem Of The Day/April 2025/april19.py
+++ Leetcode/Problem Of The Day/April 2025/april19.py
@@ -1,3 +1,20 @@
 #Question :
 #Link to the question: 
 
+from bisect import bisect_left,bisect_right
+class Solution(object):
+    def countFairPairs(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: int
+        """
+        nums.sort()
+        ans = 0
+        n = len(nums)
+        for i in range(n-1):
+            lb = bisect_left(nums,lower-nums[i],i+1)
+            ub = bisect_right(nums,upper-nums[i],i+1)
+            ans +=(ub-lb)
+        return ans
