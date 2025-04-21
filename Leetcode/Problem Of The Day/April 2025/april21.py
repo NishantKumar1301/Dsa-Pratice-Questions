235cc6d076a799f9882ee0bc48f67bbe3b2963bf
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-21 14:05:55 +0530

    Solved the potd of 21th april

--- Leetcode/Problem Of The Day/April 2025/april21.py
+++ Leetcode/Problem Of The Day/April 2025/april21.py
@@ -1,3 +1,25 @@
-#Question :
-#Link to the question: 
+#Question : Count the Hidden Sequences
+#Link to the question: https://leetcode.com/problems/count-the-hidden-sequences/description/?envType=daily-question&envId=2025-04-21
+
+class Solution(object):
+    def numberOfArrays(self, differences, lower, upper):
+        """
+        :type differences: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: int
+        """
+        x = y = cur = 0
+        for d in differences:
+            cur += d
+            x = min(x, cur)
+            y = max(y, cur)
+            if y - x > upper - lower:
+                return 0
+        return (upper - lower) - (y - x) + 1
+
+
+
+
+
 
