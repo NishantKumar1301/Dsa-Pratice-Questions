6749021980a600df3ac1f19f158bcb62db8fc0ec
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-26 14:49:43 +0530

    Solved the potd of 26th april

--- Leetcode/Problem Of The Day/April 2025/april26.py
+++ Leetcode/Problem Of The Day/April 2025/april26.py
@@ -1,26 +1,27 @@
 #Question : Count Subarrays With Fixed Bounds
 #Link to the question: https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/
 
+from collections import deque
 class Solution(object):
     def countSubarrays(self, nums, minK, maxK):
-        count = 0
+        ans = 0
         left = 0
-        dq_min = deque()
-        dq_max = deque()
+        min_queue = deque()
+        max_queue = deque()
         for i in range(len(nums)):
             if nums[i] < minK or nums[i] > maxK:
-                dq_min.clear()
-                dq_max.clear()
+                min_queue.clear()
+                max_queue.clear()
                 left = i + 1
                 continue
-            while dq_min and nums[dq_min[-1]] >= nums[i]:
-                dq_min.pop()
-            dq_min.append(i)
-            while dq_max and nums[dq_max[-1]] <= nums[i]:
-                dq_max.pop()
-            dq_max.append(i)
-            if nums[dq_min[0]] == minK and nums[dq_max[0]] == maxK:
-                start = min(dq_min[0], dq_max[0])
-                count += (start - left + 1)
-        return count
+            while min_queue and nums[min_queue[-1]] >= nums[i]:
+                min_queue.pop()
+            min_queue.append(i)
+            while max_queue and nums[max_queue[-1]] <= nums[i]:
+                max_queue.pop()
+            max_queue.append(i)
+            if nums[min_queue[0]] == minK and nums[max_queue[0]] == maxK:
+                start = min(min_queue[0], max_queue[0])
+                ans += (start - left + 1)
+        return ans
 
