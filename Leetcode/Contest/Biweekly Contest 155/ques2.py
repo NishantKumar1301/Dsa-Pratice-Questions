f992359f8f33089ac858ea995b33a35c52797db3
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-26 20:49:48 +0530

    Solved the second question of the contest

--- Leetcode/Contest/Biweekly Contest 155/ques2.py
+++ Leetcode/Contest/Biweekly Contest 155/ques2.py
@@ -1,25 +1,31 @@
-#Question : Find the Number of Copy Arrays
-#Link to the question: https://leetcode.com/contest/biweekly-contest-151/problems/find-the-number-of-copy-arrays/description/?slug=transform-array-by-parity&region=global_v2
+#Question : Unit Conversion I
+#Link to the question: https://leetcode.com/contest/biweekly-contest-155/problems/unit-conversion-i/description/?slug=find-the-most-common-response&region=global_v2
+
+MOD = 10**9 + 7
 
 class Solution(object):
-    def countArrays(self, original, bounds):
+    def dfs(self, edge):
+        for vert, fact in grid[edge]:
+            if adj[vert] == -1:
+                adj[vert] = (adj[edge] * fact) % MOD
+                self.dfs(vert)
+    
+    def baseUnitConversions(self, conversions):
         """
-        :type original: List[int]
-        :type bounds: List[List[int]]
-        :r type: int
+        :type conversions: List[List[int]]
+        :rtype: List[int]
         """
-        n = len(original)
-        copy = [0]*n
-        for i in range(n):
-            copy[i]=original[i]-original[0]
-
-        low = bounds[0][0]-copy[0]
-        high = bounds[0][1]-copy[0]
-        for i in range(1,n):
-            low = max(low,bounds[i][0]-copy[i])
-            high = min(high,bounds[i][1]-copy[i])
-            if low > high:
-                return 0
-        return max(0,high-low+1)
+        global grid, adj
+        n=0
+        for edge in conversions:
+            n = max(n, max(edge[0], edge[1]))
+        n += 1
+        grid = [[] for _ in range(n)]
+        for u, v, fact in conversions:
+            grid[u].append((v, fact))
+        adj = [-1] * n
+        adj[0] = 1
         
-
+        self.dfs(0)
+        ans = [int(x) for x in adj]
+        return ans
