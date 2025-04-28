e11bbab683d8f393fea0dc84f5444ccf8b421d69
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-28 17:18:14 +0530

    Solved the potd of 28th april

--- Leetcode/Problem Of The Day/April 2025/april28.py
+++ Leetcode/Problem Of The Day/April 2025/april28.py
@@ -1,3 +1,21 @@
-#Question :
-#Link to the question: 
+#Question : Count Subarrays With Score Less Than K
+#Link to the question: https://leetcode.com/problems/count-subarrays-with-score-less-than-k/description/?envType=daily-question&envId=2025-04-28
+
+class Solution(object):
+    def countSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        ans, total = 0, 0
+        i = 0
+        for j in range(n):
+            total += nums[j]
+            while i <= j and total * (j - i + 1) >= k:
+                total -= nums[i]
+                i += 1
+            ans += j - i + 1
+        return ans
 
