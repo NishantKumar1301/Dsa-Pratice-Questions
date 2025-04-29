20287fb8e54986b6916e301c1454fc6d179256e8
Nishant Kumar <nishantkumarfps@gmail.com>
2025-04-29 15:50:25 +0530

    Solved the potd of 29th april

--- Leetcode/Problem Of The Day/April 2025/april29.py
+++ Leetcode/Problem Of The Day/April 2025/april29.py
@@ -1,3 +1,16 @@
-#Question :
-#Link to the question: 
+#Question : Count Subarrays Where Max Element Appears at Least K Times
+#Link to the question: https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/?envType=daily-question&envId=2025-04-29
+
+class Solution:
+    def countSubarrays(self, nums, k):
+        maxi = max(nums)
+        idx = []
+        ans = 0
+        for index, element in enumerate(nums):
+            if element == maxi:
+                idx.append(index)
+            freq = len(idx)
+            if freq >= k:
+                ans += idx[-k] + 1
+        return ans
 
