78edaa67e8795e09583a2dfdab7c08ba9e400aa9
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-03 08:58:25 +0530

    Solved the potd of 3nd May

---
+++ Leetcode/Problem Of The Day/May 2025/may3.py
@@ -0,0 +1,21 @@
+#Question : Minimum Domino Rotations For Equal Row
+#Link to the question: https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/description/?envType=daily-question&envId=2025-05-03
+
+class Solution(object):
+    def minDominoRotations(self, tops, bottoms):
+        res = float('inf')
+        for x in range(1, 7):
+            res = min(res, self.getRotation(tops, bottoms, x))
+            res = min(res, self.getRotation(bottoms, tops, x))
+        return -1 if res == float('inf') else res
+
+    def getRotation(self, tops, bottoms, target):
+        rotations = 0
+        for i in range(len(tops)):
+            if tops[i] == target:
+                continue
+            if bottoms[i] == target:
+                rotations += 1
+            else:
+                return float('inf')
+        return rotations
