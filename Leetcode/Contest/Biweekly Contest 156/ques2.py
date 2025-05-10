d11cfa21c4d833227ffa5881e367ca4275fb93d3
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-10 20:23:16 +0530

    Solved the second question of the contest

---
+++ Leetcode/Contest/Biweekly Contest 156/ques2.py
@@ -0,0 +1,4 @@
+#Question : Minimum Operations to Convert All Elements to Zero
+#Link to the question: https://leetcode.com/contest/biweekly-contest-156/problems/minimum-operations-to-convert-all-elements-to-zero/description/?slug=find-most-frequent-vowel-and-consonant&region=global_v2
+
+
