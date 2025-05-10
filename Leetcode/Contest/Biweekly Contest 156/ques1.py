982180802147611420527030c3e3a43bd61df796
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-10 20:20:46 +0530

    Solved the first question of the contest

--- Leetcode/Contest/Biweekly Contest 156/ques1.py
+++ Leetcode/Contest/Biweekly Contest 156/ques1.py
@@ -1,2 +1,18 @@
-#Question :
-#Link to the question: 
+#Question : Find Most Frequent Vowel and Consonant
+#Link to the question: https://leetcode.com/contest/biweekly-contest-156/problems/find-most-frequent-vowel-and-consonant/description/
+from collections import Counter
+class Solution(object):
+    def maxFreqSum(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        mp = Counter(s)
+        v =c= 0
+        vowel = set("aeiou")
+        for char, cnt in mp.items():
+            if char in vowel:
+                v = max(v, cnt)
+            else:
+                c = max(c, cnt)
+        return v + c
