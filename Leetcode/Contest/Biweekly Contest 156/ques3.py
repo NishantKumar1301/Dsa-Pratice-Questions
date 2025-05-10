8ca73548ecaadc64ae9f692a82c1f2fc86c55dd2
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-10 21:31:10 +0530

    Solved the third  question of the contest

--- Leetcode/Contest/Biweekly Contest 156/ques3.py
+++ Leetcode/Contest/Biweekly Contest 156/ques3.py
@@ -1,8 +1,5 @@
-#Question : Maximum Weighted K-Edge Path
-#Link to the question: https://leetcode.com/contest/biweekly-contest-156/problems/maximum-weighted-k-edge-path/description/?slug=minimum-operations-to-convert-all-elements-to-zero&region=global_v2
-
 class Solution(object):
-    def maxWeight(self, n, edges, k, t):
+    def maxWeight(self, n, edge, p, k):
         """
         :type n: int
         :type edges: List[List[int]]
@@ -10,22 +7,22 @@ class Solution(object):
         :type t: int
         :rtype: int
         """
-        adj = []
-        for _ in range(n):
-            adj.append([])
-        for u, v, w in edges:
-            adj[u].append((v, w))
-        dp = [[-1] * n for _ in range(k + 1)]
-        for j in range(n):
-            dp[0][j] = 0
-        for i in range(1, k + 1):
-            for j in range(n):
-                if dp[i - 1][j] >= 0:
-                    for v, w in adj[j]:
-                        total = dp[i - 1][j] + w
-                        if total < t:
-                            dp[i][v] = max(dp[i][v], total)
+        adj = [[] for _ in range(n)]
+        for i, j, w in edge:
+            adj[j].append((i, w))
+        dp = [set([0]) for _ in range(n)]
+        for _ in range(p):
+            dp1 = [set() for _ in range(n)]
+            for node in range(n):
+                for ngbr, cost in adj[node]:
+                    for total in dp[ngbr]:
+                        combined = total + cost
+                        if combined < k:
+                            dp1[node].add(combined)
+            dp = dp1
         ans = -1
-        for i in range(n):
-            ans = max(ans, dp[k][i])
+        for node in range(n):
+            for val in dp[node]:
+                if val < k:
+                    ans = max(ans, val)
         return ans
