757d43c0fcb902b95d87e51d48253b76816a8853
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-11 08:14:56 +0530

    Solved the second question of the contest

---
+++ Leetcode/Contest/Weekly Contest 449/ques2.py
@@ -0,0 +1,4 @@
+#Question : Equal Sum Grid Partition I
+#Link to the question: https://leetcode.com/contest/weekly-contest-449/problems/equal-sum-grid-partition-i/description/?slug=minimum-deletions-for-at-most-k-distinct-characters&region=global_v2
+
+
