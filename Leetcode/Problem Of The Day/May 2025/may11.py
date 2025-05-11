b820890c6f19e98a9bbf3ac5ab80d5df77c9d113
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-11 08:24:37 +0530

    Solved the second question of the contest

--- Leetcode/Problem Of The Day/May 2025/may11.py
+++ Leetcode/Problem Of The Day/May 2025/may11.py
@@ -1,4 +1,19 @@
 #Question : Three Consecutive Odds
 #Link to the question: https://leetcode.com/problems/three-consecutive-odds/description/?envType=daily-question&envId=2025-05-11
 
-
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        n = len(arr)
+        cnt=0
+        for i in range(n):
+            if arr[i]%2!=0:
+                cnt+=1
+                if cnt==3:
+                    return True
+            else:
+                cnt =0
+        return False
