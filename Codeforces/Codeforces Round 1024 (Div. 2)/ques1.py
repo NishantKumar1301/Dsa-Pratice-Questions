35bc2aa27a3144565c4e2e22c644fdf059cfffd4
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-11 20:26:09 +0530

    Solved the first question of the contest

--- Codeforces/Codeforces Round 1024 (Div. 2)/ques1.py
+++ Codeforces/Codeforces Round 1024 (Div. 2)/ques1.py
@@ -1,2 +1,21 @@
-#Question :
-#Link to the question: 
+#Question : Dinner Time
+#Link to the question: https://codeforces.com/contest/2102/problem/A
+
+# Contest: Codeforces Round 1024 (Div. 2)
+# Date: May 11, 2025
+# Author: Nishant Kumar
+
+def solve():
+    n,m,p,q = map(int, input().split())
+    if (n%p==0 and (m != (n//p)*q)):
+        print("NO")
+    else:
+        print("YES")
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        solve()
+        
+# Author: Nishant Kumar
+
+
+
