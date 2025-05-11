95885a328965343b3a2044159f316f539a58a48b
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-11 08:03:11 +0530

    Solved the first question of the contest

--- Leetcode/Contest/Weekly Contest 449/ques1.py
+++ Leetcode/Contest/Weekly Contest 449/ques1.py
@@ -1,2 +1,4 @@
-#Question :
-#Link to the question: 
+#Question : Minimum Deletions for At Most K Distinct Characters
+#Link to the question: https://leetcode.com/contest/weekly-contest-449/problems/minimum-deletions-for-at-most-k-distinct-characters/description/
+
+
