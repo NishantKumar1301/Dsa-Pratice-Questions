3bad9f3d022eb7679529d84dba3350bd2c7f3727
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-11 08:12:55 +0530

    Solved the first question of the contest

--- Leetcode/Contest/Weekly Contest 449/ques1.py
+++ Leetcode/Contest/Weekly Contest 449/ques1.py
@@ -1,4 +1,21 @@
 #Question : Minimum Deletions for At Most K Distinct Characters
 #Link to the question: https://leetcode.com/contest/weekly-contest-449/problems/minimum-deletions-for-at-most-k-distinct-characters/description/
-
-
+from collections import Counter
+class Solution(object):
+    def minDeletion(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        # s= set(s)
+        # return len(s)-k
+        freq = Counter(s)
+        if len(freq) <= k:
+            return 0
+        freq = sorted(freq.values())
+        ans = 0
+        n = len(freq) - k
+        for i in range(n):
+            ans += freq[i]
+        return ans
