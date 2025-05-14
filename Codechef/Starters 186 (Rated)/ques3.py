788a2048c0c71201f4fc105a2aea718a72ce7e0c
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-14 20:50:03 +0530

    Solved the third question of the contest

--- Codechef/Starters 186 (Rated)/ques3.py
+++ Codechef/Starters 186 (Rated)/ques3.py
@@ -1,2 +1,17 @@
 #Question : Distinct Arrays
 #Link to the question: https://www.codechef.com/START186B/problems/DISTARR
+
+MOD = 998244353
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    ans = 1
+    for i in range(n):
+        p = arr[i] - i
+        if p <= 0:
+            ans = 0
+            break
+        ans = ((ans) * (p % MOD)) % MOD
+    print(ans)
+
