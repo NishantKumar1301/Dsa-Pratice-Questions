90a23272200b211c9d57c394f14da9486864e8a2
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-14 20:15:14 +0530

    Solved the first question of the contest

--- Codechef/Starters 186 (Rated)/ques1.py
+++ Codechef/Starters 186 (Rated)/ques1.py
@@ -1,2 +1,20 @@
 #Question : Approval Ratings
 #Link to the question: https://www.codechef.com/START186B/problems/APPROVAL
+
+
+# cook your dish here
+for _ in range(int(input())):
+    arr = list(map(int,input().split()))
+    total = sum(arr)
+    ans = 0
+    if total >= 35:
+        print(ans)
+        continue
+    arr.sort()
+    for num in arr:
+        if total >= 35:
+            break
+        total += (10 - num)
+        ans += 100
+    print(ans)
+
