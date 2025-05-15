3f786a3537b4b65683076a8f40b1b5741d617ec4
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-15 15:34:29 +0530

    Solved the potd of 15th may

--- Leetcode/Problem Of The Day/May 2025/may15.py
+++ Leetcode/Problem Of The Day/May 2025/may15.py
@@ -1,2 +1,15 @@
 #Question : Longest Unequal Adjacent Groups Subsequence I
 #Link to the question: https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i/description/?envType=daily-question&envId=2025-05-15
+
+class Solution(object):
+    def getLongestSubsequence(self, words, groups):
+        """
+        :type words: List[str]
+        :type groups: List[int]
+        :rtype: List[str]
+        """
+        ans = []
+        for i in range(len(words)):
+            if i==0 or (groups[i]!=groups[i-1]):
+                ans.append(words[i])
+        return ans
