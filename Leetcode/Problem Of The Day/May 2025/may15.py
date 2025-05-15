b98a65f842ba36986e301607c3afeb1c3733daf5
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-15 15:34:05 +0530

    Solved the potd of 15th may

---
+++ Leetcode/Problem Of The Day/May 2025/may15.py
@@ -0,0 +1,2 @@
+#Question : Longest Unequal Adjacent Groups Subsequence I
+#Link to the question: https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i/description/?envType=daily-question&envId=2025-05-15
