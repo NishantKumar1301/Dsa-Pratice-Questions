43739d69188ffe29021c4d93b66ca18e21a5622b
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-17 21:50:45 +0530

    Splved the 3rd question of the contest

---
+++ Codeforces/Codeforces Round 1025 (Div. 2)/ques3.py
@@ -0,0 +1,30 @@
+#Question : Hacking Numbers (Easy Version)
+#Link to the question: https://codeforces.com/contest/2109/problem/C1
+
+# Contest: Codeforces Round 1025 (Div. 2)
+# Date: May 17, 2025
+# Author: Nishant Kumar
+
+def solve():
+    z = int(input())
+    print()
+    print("mul 9", flush=True)
+    __ = int(input())
+    print()
+    print("digit", flush=True)
+    __ = int(input())
+    print()
+    print("digit", flush=True)
+    __ = int(input())
+    print()
+    print(f"add {z - 9}", flush=True)
+    __ = int(input())
+    print()
+    print("!", flush=True)
+    __ = int(input())
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        solve()
+
+# Author: Nishant Kumar
