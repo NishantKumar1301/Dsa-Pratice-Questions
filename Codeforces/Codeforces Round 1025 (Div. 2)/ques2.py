cb1843da40a6a0d951cf027e833cdcb8c2773fc1
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-17 21:22:22 +0530

    Solved the second question of the contest

--- Codeforces/Codeforces Round 1025 (Div. 2)/ques2.py
+++ Codeforces/Codeforces Round 1025 (Div. 2)/ques2.py
@@ -5,7 +5,7 @@
 # Date: May 17, 2025
 # Author: Nishant Kumar
 
-def helper(t, p):
+def solve1(t, p):
     ans = 0
     m = 2
     while t > 1:
@@ -20,8 +20,8 @@ def solve():
         n, m, a, b = map(int, input().split())
         row = min(a, n - a + 1)
         col = min(b, m - b + 1)
-        ans1 = helper(m, row) +1
-        ans2 = helper(n, col)  +1 
+        ans1 = solve1(m, row) +1
+        ans2 = solve1(n, col)  +1 
         print(min(ans1 , ans2))
 
 if __name__ == "__main__":
