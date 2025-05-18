4e3bdbe82274b43ea0bd3b069cbf3d6a2d7ebdbf
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-18 08:22:36 +0530

    Solved the first question of the contest

---
+++ Leetcode/Contest/Weekly Contest 450/ques1.py
@@ -0,0 +1,2 @@
+#Question : Smallest Index With Digit Sum Equal to Index
+#Link to the question: https://leetcode.com/contest/weekly-contest-450/problems/smallest-index-with-digit-sum-equal-to-index/description/
