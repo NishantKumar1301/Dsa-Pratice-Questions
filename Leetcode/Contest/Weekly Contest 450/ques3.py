d71850b0e36c49909f2dd4ffd97e3981f372ec06
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-18 08:57:28 +0530

    Solved the third question of the contest

--- Leetcode/Contest/Weekly Contest 450/ques3.py
+++ Leetcode/Contest/Weekly Contest 450/ques3.py
@@ -1,2 +1,4 @@
-#Question :
-#Link to the question : 
+#Question : Grid Teleportation Traversal
+#Link to the question : https://leetcode.com/contest/weekly-contest-450/problems/grid-teleportation-traversal/?slug=minimum-swaps-to-sort-by-digit-sum&region=global_v2
+
+
