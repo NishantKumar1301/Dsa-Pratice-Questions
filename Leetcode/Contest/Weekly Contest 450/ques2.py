9bb86dd878f276d30310a36c6cb29a83d5b8ed73
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-18 08:25:42 +0530

    Added the second question [https://leetcode.com/contest/weekly-contest-450/problems/minimum-swaps-to-sort-by-digit-sum/description/?slug=smallest-index-with-digit-sum-equal-to-index&region=global_v2]

--- Leetcode/Contest/Weekly Contest 450/ques2.py
+++ Leetcode/Contest/Weekly Contest 450/ques2.py
@@ -1,2 +1,4 @@
-#Question :
-#Link to the question: 
+#Question : Minimum Swaps to Sort by Digit Sum
+#Link to the question: https://leetcode.com/contest/weekly-contest-450/problems/minimum-swaps-to-sort-by-digit-sum/description/?slug=smallest-index-with-digit-sum-equal-to-index&region=global_v2
+
+
