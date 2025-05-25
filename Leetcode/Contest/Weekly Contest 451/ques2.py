9b65e0d9550de3748691dced8dd1e77b46a28704
Nishant Kumar <nishantkumarfps@gmail.com>
2025-05-25 08:25:06 +0530

    Solved the second question of the contest

--- Leetcode/Contest/Weekly Contest 451/ques2.py
+++ Leetcode/Contest/Weekly Contest 451/ques2.py
@@ -1,2 +1,18 @@
-#Question :
-#Link to the question: 
+#Question : Resulting String After Adjacent Removals
+#Link to the question: https://leetcode.com/contest/weekly-contest-451/problems/resulting-string-after-adjacent-removals/?slug=find-minimum-log-transportation-cost&region=global_v2
+
+class Solution(object):
+    def resultingString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        for char in s:
+            if stack:
+                x = abs(ord(char)-ord(stack[-1]))
+                if x==1 or x==25:
+                    stack.pop()
+                    continue
+            stack.append(char)
+        return ''.join(stack)
