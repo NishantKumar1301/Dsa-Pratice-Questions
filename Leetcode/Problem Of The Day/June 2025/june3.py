e441afce770b194f320f80a01f58712655f51a46
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-03 23:07:06 +0530

    Solved the potd of 3rd my

---
+++ Leetcode/Problem Of The Day/June 2025/june3.py
@@ -0,0 +1,31 @@
+#Question : Maximum Candies You Can Get from Boxes
+#Link to the question: Maximum Candies You Can Get from Boxes
+class Solution(object):
+    def maxCandies(self, status, candies, keys, containedBoxes, initialBoxes):
+        """
+        :type status: List[int]
+        :type candies: List[int]
+        :type keys: List[List[int]]
+        :type containedBoxes: List[List[int]]
+        :type initialBoxes: List[int]
+        :rtype: int
+        """
+        seen = set()
+        can_look = set()
+        def dfs(box):
+            if box in seen:
+                return 0
+            if status[box] == 0:
+                can_look.add(box)
+                return 0
+            seen.add(box)
+            total = candies[box]
+            for next_box in containedBoxes[box]:
+                total += dfs(next_box)
+            for next_box in keys[box]:
+                status[next_box] = 1
+                if next_box in can_look:
+                    total += dfs(next_box)
+            return total
+        return sum([dfs(box) for box in initialBoxes])
+        
