9b6a0bab65ef630a4fba878ded479c71a8e7e4a4
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-04 21:28:39 +0530

    Solved the first question of the contest

--- Codechef/Starters 189 (Rated)/ques1.py
+++ Codechef/Starters 189 (Rated)/ques1.py
@@ -1,2 +1,6 @@
-#Question :
-#Link to the question:
+# cook your dish here
+#Question : Pizza Party
+#Link to the question: https://www.codechef.com/START189B/problems/PIZZAPARTY
+a,b = map(int,input().split())
+a+=1
+print(((a*4+b*3+7)//8))
