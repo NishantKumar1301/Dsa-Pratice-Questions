24fe2d5457c8f5449701e2a14dfb7d9239fd88cb
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-04 21:30:30 +0530

    Solved the third question of the contest

---
+++ Codechef/Starters 189 (Rated)/ques3.py
@@ -0,0 +1,26 @@
+# cook your dish here
+#Question : Distinct 2 Subarray
+#Link to the question: https://www.codechef.com/START189B/problems/DIS2SUB
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    p = n+1
+    ans =p 
+    for i in range(n):
+        freq ,diff= [0] * 101, 0
+        for j in range(i, n):
+            x = arr[j]
+            if freq[x] == 0:
+                diff += 1
+            freq[x] += 1
+            if diff == 2:
+                win_length = j - i + 1
+                if win_length < ans:
+                    ans = win_length
+                break
+            elif diff > 2:
+                break  
+    if ans ==p:
+        print(-1)
+    else:
+        print(ans)
