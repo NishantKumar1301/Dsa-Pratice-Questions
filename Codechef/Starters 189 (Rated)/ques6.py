388e46eb7d412eca9ef24f36b7fb9706257e8bda
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-04 21:33:14 +0530

    Solved the sixth question of the contest

---
+++ Codechef/Starters 189 (Rated)/ques6.py
@@ -0,0 +1,21 @@
+# cook your dish here
+#Question : Sortable Subarrays (Easy)
+#Link to the question: https://www.codechef.com/START189B/problems/SORTSUB7EZ
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    ans = 0
+    for i in range(n):
+        idx = -1  
+        for j in range(i, n):
+            v= arr[j]
+            h=(v - 1) // 2
+            if idx < h:
+                idx += 1
+                ans += 1
+            elif idx < v:
+                ans += 1
+                idx = v
+            else:
+                break
+    print(ans)
