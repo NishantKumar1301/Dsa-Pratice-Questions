31baacf5673cb93dc2c264fd46c6c7537fd36cd9
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-07 22:35:03 +0530

    SOlved the first problem of the contest

--- Leetcode/Contest/Biweekly Contest 158/ques1.py
+++ Leetcode/Contest/Biweekly Contest 158/ques1.py
@@ -1,2 +1,17 @@
-#Question :
-#Link to the question:
+#Question : Maximize Y‑Sum by Picking a Triplet of Distinct X‑Values
+#Link to the question:https://leetcode.com/contest/biweekly-contest-158/problems/maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/
+class Solution(object):
+    def maxSumDistinctTriplet(self, x, y):
+        """
+        :type x: List[int]
+        :type y: List[int]
+        :rtype: int
+        """
+        freq ={}
+        for i,j in zip(x,y):
+            if i not in freq or j > freq[i]:
+                freq[i]=j
+        ans = sorted(freq.values(),reverse=True)
+        if len(freq)<3:
+            return -1
+        return sum(ans[:3])
