de1a4e4c93ca08afcaf2b300f248be29f4ea6c36
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-09 23:36:42 +0530

    Solved the potd of 9th june

--- Leetcode/Problem Of The Day/June 2025/june9.py
+++ Leetcode/Problem Of The Day/June 2025/june9.py
@@ -1,2 +1,23 @@
-#Question :
-#Link to the question:
+#Question : K-th Smallest in Lexicographical Order
+#Link to the question:https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/description/
+class Solution(object):
+    def countNumber(self, curr, nxt, n):
+        cnt = 0
+        while curr <= n:
+            cnt += min(n + 1, nxt) - curr
+            curr *= 10
+            nxt *= 10
+        return cnt
+
+    def findKthNumber(self, n, k):
+        ans = 1
+        k -= 1
+        while k > 0:
+            cnt = self.countNumber(ans, ans + 1, n)
+            if k >= cnt:
+                ans += 1
+                k -= cnt
+            else:
+                ans *= 10
+                k -= 1
+        return ans
