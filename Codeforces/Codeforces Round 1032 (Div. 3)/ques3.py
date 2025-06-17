c5f68230fca322c1ecd3800a370291151d3de6fb
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-17 20:36:54 +0530

    Solved the third question of the contest

---
+++ Codeforces/Codeforces Round 1032 (Div. 3)/ques3.py
@@ -0,0 +1,15 @@
+# Contest: Codeforces Round 1032 (Div. 3)
+# Date: June 17, 2025
+# Author: Nishant Kumar
+
+def solve():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        solve()
+
+# Author: Nishant Kumar
