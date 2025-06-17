5916e69797adaa266851282e548aa47c1c0ff5ee
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-17 20:27:33 +0530

    Solved the second question of the contest

--- Codeforces/Codeforces Round 1032 (Div. 3)/ques2.py
+++ Codeforces/Codeforces Round 1032 (Div. 3)/ques2.py
@@ -1,11 +1,22 @@
+#Question :
+#Link to the question:
+
 # Contest: Codeforces Round 1032 (Div. 3)
 # Date: June 17, 2025
 # Author: Nishant Kumar
 
 def solve():
     n = int(input())
-    arr = list(map(int, input().split()))
-    
+    s= input()
+    set_ = set()
+    set_.add(s[0])
+    set_.add(s[n-1])
+    for i in range(1,n-1):
+        if s[i] in set_:
+            print("YES")
+            return 
+        set_.add(s[i])
+    print("NO")
 
 
 if __name__ == "__main__":
