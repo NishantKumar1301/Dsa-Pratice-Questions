8303527af282eaea1f57164d8d55a9ad675e8a0d
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-17 21:09:59 +0530

    Solved the third question of the contest

--- Codeforces/Codeforces Round 1032 (Div. 3)/ques3.py
+++ Codeforces/Codeforces Round 1032 (Div. 3)/ques3.py
@@ -3,11 +3,30 @@
 # Author: Nishant Kumar
 
 def solve():
-    n = int(input())
-    arr = list(map(int, input().split()))
-    
-
-
+    n, m = map(int, input().split())
+    arr= []
+    ans = 0
+    for _ in range(n):
+        row = list(map(int, input().split()))
+        arr.append(row)
+        ans = max(ans, max(row))
+    tmp = []
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == ans:
+                tmp.append((i, j))
+    flag = False
+    for i in range(n):
+        s = set()
+        for a, b in tmp:
+            if a != i:
+                s.add(b)
+                if len(s) > 1:
+                    break
+        if len(s) <= 1:
+            flag = True
+            break
+    print(ans - 1 if flag else ans)
 if __name__ == "__main__":
     for _ in range(int(input())):
         solve()
