9124292f7a9d2adb8a9a985787b83138fe4876d2
Nishant Kumar <nishantkumarfps@gmail.com>
2025-06-22 17:18:03 +0530

    Solved the mst by prims algorithm

---
+++ Faiz Interview Sheet/Graph/Graph Level 3/ques7.py
@@ -0,0 +1,30 @@
+#Question : Minimum Spanning Tree
+#Link to the question:  https://www.geeksforgeeks.org/problems/minimum-spanning-tree/1
+
+from heapq import heappush,heappop
+from typing import List
+class Solution:
+    
+    #Function to find sum of weights of edges of the Minimum Spanning Tree.
+    def spanningTree(self, V: int, adj: List[List[int]]) -> int:
+        #code here
+        mst = []
+        visited = [False]*V
+        pq = [(0,0,-1)]#wt,node,parent
+        total =0
+        while pq:
+            wt,node,parent= heappop(pq)
+            if visited[node]:
+                continue
+            visited[node]=True
+            total+=wt
+            if parent!=-1:
+                mst.append((parent,node))
+            for adjNode,edgeWt in adj[node]:
+                if not visited[adjNode]:
+                    heappush(pq,(edgeWt,adjNode,node))
+        
+        #If we have to print the mst
+        # for u,v in mst:
+        #     print(u,v)
+        return total
