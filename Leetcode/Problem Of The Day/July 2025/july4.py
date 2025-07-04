17d7fc7c07cac89bb15221a95b1bd11b7aa6d115
Nishant Kumar <nishantkumarfps@gmail.com>
2025-07-04 18:38:36 +0530

    Solved the potd of 4th july

---
+++ Leetcode/Problem Of The Day/July 2025/july4.py
@@ -0,0 +1,20 @@
+#Question : Find the K-th Character in String Game II
+#Link to the question:  https://leetcode.com/problems/find-the-k-th-character-in-string-game-ii/
+
+class Solution(object):
+    def kthCharacter(self, k, operations):
+        """
+        :type k: int
+        :type operations: List[int]
+        :rtype: str
+        """
+        ans = 0
+        while k != 1:
+            t = k.bit_length() - 1
+            if (1 << t) == k:
+                t -= 1
+            k -= 1 << t
+            if operations[t]:
+                ans += 1
+        return chr(ord("a") + (ans % 26))
+        
