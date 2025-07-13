f6eb1e93fc4278b5bb66edf034c485a6f69db34c
Nishant Kumar <nishantkumarfps@gmail.com>
2025-07-13 21:39:57 +0530

    Solved the potd of 13th july

---
+++ Leetcode/Problem Of The Day/July 2025/july13.py
@@ -0,0 +1,24 @@
+#Question : Maximum Matching of Players With Trainers
+#Link to the question: https://leetcode.com/problems/maximum-matching-of-players-with-trainers/
+
+class Solution(object):
+    def matchPlayersAndTrainers(self, players, trainers):
+        """
+        :type players: List[int]
+        :type trainers: List[int]
+        :rtype: int
+        """
+        players.sort()
+        trainers.sort()
+        m, n = len(players), len(trainers)
+        i = j = count = 0
+
+        while i < m and j < n:
+            while j < n and players[i] > trainers[j]:
+                j += 1
+            if j < n:
+                count += 1
+            i += 1
+            j += 1
+
+        return count
