b6614c4467024c89d7f18b37f379673cf7325f72
Nishant Kumar <nishantkumarfps@gmail.com>
2025-07-23 22:43:58 +0530

    Solved the potd of 23rd july

--- Leetcode/Problem Of The Day/July 2025/july23.py
+++ Leetcode/Problem Of The Day/July 2025/july23.py
@@ -5,15 +5,14 @@ class Solution(object):
     def remove_substring(self,s,matched):
         stack = []
         for char in s:
-            if stack and char==matched[1] and stack[-1]==matched[0]:
+            if stack and char==matched[1] and stack[-1]==matched[0] :
                 stack.pop()
             else:
                 stack.append(char)
         new_string = ""
         while stack:
-            new_string+= stack.pop()
+            new_string+=stack.pop()
         return new_string[::-1]
-        # return ''.join(stack)
 
     def maximumGain(self, s, x, y):
         """
@@ -29,13 +28,13 @@ class Solution(object):
         #Pass 1 
         first_string = self.remove_substring(s,max_str)
         char_removed = n - len(first_string)
-        first_pass_pair_removed = char_removed//2
-        ans += first_pass_pair_removed* max(x,y)
+        first_time_pair_removed = char_removed//2
+        ans+= first_time_pair_removed*max(x,y)
 
         #Pass 2 
         second_string = self.remove_substring(first_string,min_str)
-        char_removed = len(first_string) - len(second_string)
-        second_time_pair_removed = char_removed //2 
-        ans += second_time_pair_removed * min(x,y)
+        char_removed = len(first_string)-len(second_string)
+        second_time_pair_removed = char_removed//2
+        ans += min(x,y)*second_time_pair_removed
 
         return ans
