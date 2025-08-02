c68f9cfe9d4302c03f2757c62d35b1b1c9681f94
Nishant Kumar <nishantkumarfps@gmail.com>
2025-08-02 20:40:45 +0530

    Solved the potd of 2nd august

---
+++ Leetcode/Problem Of The Day/August 2025/august2.py
@@ -0,0 +1,32 @@
+#Question : Rearranging Fruits
+#Link to the question: https://leetcode.com/problems/rearranging-fruits
+from collections import defaultdict
+class Solution(object):
+    def minCost(self, basket1, basket2):
+        """
+        :type basket1: List[int]
+        :type basket2: List[int]
+        :rtype: int
+        """
+        freq  = defaultdict(int)
+        mini = float('inf')
+        for num in basket1:
+            freq[num]+=1
+            mini = min(mini,num)
+        for num in basket2:
+            freq[num]-=1
+            mini = min(mini,num)
+        arr = []
+        for cost,cnt in freq.items():
+            if cnt==0:
+                continue
+            if cnt%2!=0:
+                return -1
+            arr.append(cost*abs(cnt//2))
+        arr.sort()
+        ans =0
+        n = len(arr)
+        for i in range(n//2):
+            ans+=min(arr[i],mini*2)
+        return ans
+
