ec075afb1d08868579b055c9251bd3a460d48409
Nishant Kumar <nishantkumarfps@gmail.com>
2025-08-12 19:02:49 +0530

    Solved the search of a node in bst

---
+++ Faiz Interview Sheet/Binary Search Tree/ques1.py
@@ -0,0 +1,25 @@
+#Question : Search in a Binary Search Tree
+#Link to the question: https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+# Definition for a binary tree node.
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        while root and root.val!=val:
+            if val<root.val:
+                root = root.left
+            else:
+                root = root.right
+        return root
