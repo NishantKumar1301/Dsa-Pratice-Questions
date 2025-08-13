dc27230c829d418cb76851fe9d7371c2a02da96c
Nishant Kumar <nishantkumarfps@gmail.com>
2025-08-13 22:05:31 +0530

    Solved the potd of 13th august

---
+++ Leetcode/Problem Of The Day/August 2025/august13.py
@@ -0,0 +1,17 @@
+#Question : Power of Three
+#Link to the question: https://leetcode.com/problems/power-of-three/
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n==0:
+            return False
+        while n %3 ==0:
+            n = n //3
+        if n ==1:
+            return True
+        else:
+            return False
+        
