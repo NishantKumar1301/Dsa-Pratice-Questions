4813c9718b3735c8706d61280a88490232beef3e
Nishant Kumar <nishantkumarfps@gmail.com>
2025-08-29 22:12:42 +0530

    SOlved the potd of 29th august

---
+++ Leetcode/Problem Of The Day/August 2025/august29.py
@@ -0,0 +1,11 @@
+#Question : Alice and Bob Playing Flower Game
+#Link to the question: https://leetcode.com/problems/alice-and-bob-playing-flower-game/ 
+class Solution(object):
+    def flowerGame(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        return (m * n) // 2
+        
