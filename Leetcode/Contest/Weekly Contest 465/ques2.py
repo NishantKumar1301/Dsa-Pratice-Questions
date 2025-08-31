fd71aac62c05b4d1006aae1b8bf0f554b8a05302
Nishant Kumar <nishantkumarfps@gmail.com>
2025-08-31 09:00:00 +0530

    Solved the second question of the contest

--- Leetcode/Contest/Weekly Contest 465/ques2.py
+++ Leetcode/Contest/Weekly Contest 465/ques2.py
@@ -1,23 +1,20 @@
-class Solution(object):
+#Question : Maximum Product of Two Integers With No Common Bits
+#Link to the question: https://leetcode.com/contest/weekly-contest-465/problems/maximum-product-of-two-integers-with-no-common-bits/
+class Solution:
     def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        maxi,ans = max(nums),0
-        c = max(1, maxi.bit_length())   
-        d = 1 << c
-        e = d - 1
-        dp = [-1] * d
-        for x in nums:
-            dp[x] = max(dp[x], x)
-        for b in range(c):
-            for m in range(d):
-                if (1 << b) & m:
-                    dp[m] = max(dp[m], dp[m ^ (1 << b)])
-        ans = 0
-        for x in nums:
-            y = dp[e ^ x]
-            if y != -1:
-                ans = max(ans, x * y)
+        maxi , ans  = max(nums) , 0
+        n = max(1, maxi.bit_length())
+        m = 1 << n
+        p = m - 1
+        dp = [-1] * m
+        for num in nums:
+            dp[num] = max(dp[num], num)
+        for i in range(n):
+            for j in range(m):
+                if j & (1 << i):
+                    dp[j] = max(dp[j], dp[j ^ (1 << i)])
+        for num in nums:
+            arr = dp[p ^ num]
+            if arr != -1:
+                ans = max(ans, num * arr)
         return ans
