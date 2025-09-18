671b633e60db21c1111c5b2d17de91f59eae0855
Nishant Kumar <nishantkumarfps@gmail.com>
2025-09-18 14:06:07 +0530

    Solved the potd of 18th september

---
+++ Leetcode/Problem Of The Day/September 2025/sept18.py
@@ -0,0 +1,70 @@
+#Question : Design Task Manager
+#Link to the question: https://leetcode.com/problems/design-task-manager
+
+import heapq
+
+
+class TaskManager(object):
+
+    def __init__(self, tasks):
+        """
+        :type tasks: List[List[int]]
+        """
+        self.heap = []
+        self.taskPriority = {}
+        self.taskOwner = {}
+        for t in tasks:
+            self.add(t[0], t[1], t[2])
+        
+
+    def add(self, userId, taskId, priority):
+        """
+        :type userId: int
+        :type taskId: int
+        :type priority: int
+        :rtype: None
+        """
+        heapq.heappush(self.heap, (-priority, -taskId))
+        self.taskPriority[taskId] = priority
+        self.taskOwner[taskId] = userId
+        
+
+    def edit(self, taskId, newPriority):
+        """
+        :type taskId: int
+        :type newPriority: int
+        :rtype: None
+        """
+        heapq.heappush(self.heap, (-newPriority, -taskId))
+        self.taskPriority[taskId] = newPriority
+        
+
+    def rmv(self, taskId):
+        """
+        :type taskId: int
+        :rtype: None
+        """
+        self.taskPriority[taskId] = -1
+        
+
+    def execTop(self):
+        """
+        :rtype: int
+        """
+        while self.heap:
+            negp, negid = heapq.heappop(self.heap)
+            p = -negp
+            tid = -negid
+            if self.taskPriority.get(tid, -2) == p:
+                self.taskPriority[tid] = -1
+                return self.taskOwner.get(tid, -1)
+        return -1
+        
+
+
+# Your TaskManager object will be instantiated and called as such:
+# obj = TaskManager(tasks)
+# obj.add(userId,taskId,priority)
+# obj.edit(taskId,newPriority)
+# obj.rmv(taskId)
+# param_4 = obj.execTop()
