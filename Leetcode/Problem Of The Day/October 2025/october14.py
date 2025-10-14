4ee2a6a4de22c16aac004e5646bee485ee56a573
Nishant Kumar <nishantkumarfps@gmail.com>
2025-10-14 19:40:01 +0530

    Solved the potd of 14th october

---
+++ Leetcode/Problem Of The Day/October 2025/october14.py
@@ -0,0 +1,19 @@
+#Question : Adjacent Increasing Subarrays Detection I
+#Link to the question: https://leetcode.com/problems/adjacent-increasing-subarrays-detection-i/
+class Solution(object):
+    def hasIncreasingSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        n = len(nums)
+        cnt, precnt, ans = 1, 0, 0
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                cnt += 1
+            else:
+                precnt, cnt = cnt, 1
+            ans = max(ans, min(precnt, cnt))
+            ans = max(ans, cnt // 2)
+        return ans >= k
